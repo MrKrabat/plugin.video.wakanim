32712d5b39942bcd5cbea1fbfe4dc0ab161b7a57
MrKrabat <MrKrabat@users.noreply.github.com>
2017-09-22 15:17:14 +0200

    Initial release
    Features:
    - Login with your account
    - Search for animes
    - Show "My Downloads"
    - Show "My Collection"
    - Show/browse all available anime
    - Show/browse all seasons/arcs of an anime
    - Show/browse all episodes of an season/arc
    - Display various informations (Kodi displays not all we provide)
    - Watch videos with premium subscription

---
+++ README.md
@@ -0,0 +1,47 @@
+# Wakanim plugin for Kodi
+
+Wakanim
+A KODI (XBMC) plugin for Wakanim. 
+
+Git Repo: https://github.com/MrKrabat/plugin.video.wakanim_de
+
+Forum Posting: --
+
+**This Plugin is ONLY for german visitors of Wakanim.tv!**
+
+**WARNING: You MUST be a PREMIUM member OR buy videos to use this plugin!**
+***
+
+Compatibility
+
+| Device  | Kodi | Status |
+| ------------- | ------------- | ------------- |
+| Windows | 17.x  | Yes  |
+| Linux | 17.x  | Yes  |
+| MacOS | 17.x  | untested  |
+| Android | 17.x  | Yes  |
+| RaspberryPi | 17.x  | Yes  |
+| iOS | 17.x  | untested  |
+
+Devices running an older Kodi version will only support videos you bought.
+***
+
+What this plugin currently can do:
+- [x] Login with your account
+- [x] Search for animes
+- [x] Show "My Downloads"
+- [x] Show "My Collection"
+- [x] Show/browse all available anime
+- [x] Show/browse all seasons/arcs of an anime
+- [x] Show/browse all episodes of an season/arc
+- [x] Display various informations (Kodi displays not all we provide)
+- [x] Watch videos with premium subscription
+
+What is planned for the future:
+- [ ] Watch videos you have bought
+- [ ] Select video resolution (only for videos you bought)
+
+***
+
+_Kodi® (formerly known as XBMC™) is a registered trademark of the XBMC Foundation. 
+This website and addon is not affiliated with Kodi, Team Kodi, or the XBMC Foundation._
---
+++ resources/lib/wakanim.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# Wakanim - Watch videos from the german anime platform Wakanim.tv on Kodi.
+# Copyright (C) 2017 MrKrabat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+import xbmc
+import xbmcgui
+import xbmcplugin
+
+import cmdargs
+import login
+import netapi
+import list
+
+
+def main():
+	"""Main function for the addon
+	"""
+	args = cmdargs.parse_args()
+
+	# check if account is set
+	username = args._addon.getSetting("wakanim_username")
+	password = args._addon.getSetting("wakanim_password")
+
+	if username == "" or password == "":
+		# open addon settings
+		args._addon.openSettings()
+		return False
+	else:
+		# login
+		success = login.login(username, password, args)
+		if success==True:
+			# list menue
+			xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+			check_mode(args)
+		else:
+			# login failed
+			xbmc.log("[PLUGIN] %s: Login failed" % args._addonname, xbmc.LOGERROR)
+			xbmcgui.Dialog().ok(args._addonname, args._addon.getLocalizedString(30040))
+			return False
+
+
+def check_mode(args):
+	"""Run mode-specific functions
+	"""
+	mode = args.mode
+
+	if mode is None:
+		showMainMenue(args)
+	elif mode == 'catalog':
+		netapi.showCatalog(args)
+	elif mode == 'search':
+		netapi.searchAnime(args)
+	elif mode == 'downloads':
+		netapi.myDownloads(args)
+	elif mode == 'collection':
+		netapi.myCollection(args)
+	elif mode == 'list_season':
+		netapi.listSeason(args)
+	elif mode == 'list_episodes':
+		netapi.listEpisodes(args)
+	elif mode == 'videoplay':
+		netapi.startplayback(args)
+	else:
+		# unkown mode
+		xbmc.log("[PLUGIN] %s: Failed in check_mode '%s'" % (args._addonname, str(mode)), xbmc.LOGERROR)
+		xbmcgui.Dialog().notification(args._addonname, args._addon.getLocalizedString(30041), xbmcgui.NOTIFICATION_ERROR)
+		showMainMenue(args)
+
+
+def showMainMenue(args):
+	"""Show main menu
+	"""
+	list.add_item(args,
+			{'title':	args._addon.getLocalizedString(30020),
+			'mode':		'catalog'})
+	list.add_item(args,
+			{'title':	args._addon.getLocalizedString(30021),
+			'mode':		'search'})
+	list.add_item(args,
+			{'title':	args._addon.getLocalizedString(30022),
+			'mode':		'downloads'})
+	list.add_item(args,
+			{'title':	args._addon.getLocalizedString(30023),
+			'mode':		'collection'})
+	list.endofdirectory()
